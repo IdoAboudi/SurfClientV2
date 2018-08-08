b9d5365be7668fddad4e16c8ed815a42e6c143bf
or_p <or.p@reflectiz.com>
2018-08-08 23:57:12 +0300

    login with db

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,34 @@
 import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService{
 
-  isUserLoggedIn: boolean = false
+  isUserLoggedIn: boolean = false;
+  result;
+  private loginUrl = 'http://localhost:8080/api/login/:username/:password';
+  constructor(private http: Http){
+  }
 
   isLoggedIn(){
     return this.isUserLoggedIn;
   }
 
-  login(user: string,pass: string){
-    console.log(`auth service got ${user} pass ${pass}`);
-    if(user=="user" && pass=="s"){
-      console.log(`equals`);
-      this.isUserLoggedIn = true
-      return true;
-    } else return false;
+  login(user: string,pass: string, callback: (boolean) => void){
+    this.result = this.http.get(this.loginUrl.replace(":username",user).replace(":password",pass));
+
+    this.result.subscribe((res) => {
+      console.log(`service subscriber`);
+      if(res.text() === 'TRUE'){
+        this.isUserLoggedIn = true;
+        callback(true);
+      } else {
+        this.isUserLoggedIn = false;
+        callback(false);
+      }
+    });
+
+    return this.result;
   }
 }
